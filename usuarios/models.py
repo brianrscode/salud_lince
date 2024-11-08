9f50d70aa0b54985163ea389f67c4cf29deb6c30
brianrscode <135724962+brianrscode@users.noreply.github.com>
2024-11-08 15:56:40 -0600

    Agrego usuarios

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,21 +5,21 @@ class Usuario(AbstractUser):
     pass
 
 class Medico(models.Model):
+    matricula = models.CharField(primary_key=True, max_length=6, unique=True)
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
-    matricula = models.CharField(max_length=6, unique=True)
-    nombre = models.CharField(max_length=50)
-    apellido_paterno = models.CharField(max_length=50)
-    apellido_materno = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=255)
+    apellido_paterno = models.CharField(max_length=255)
+    apellido_materno = models.CharField(max_length=255)
     correo = models.EmailField(unique=True)
-    contrasena = models.CharField(max_length=100)
+    contrasena = models.CharField(max_length=255)
     fecha_nacimiento = models.DateField()
 
 class Paciente(models.Model):
+    matricula = models.CharField(primary_key=True, max_length=6, unique=True)
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
-    matricula = models.CharField(max_length=6, unique=True)
-    nombre = models.CharField(max_length=50)
-    apellido_paterno = models.CharField(max_length=50)
-    apellido_materno = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=255)
+    apellido_paterno = models.CharField(max_length=255)
+    apellido_materno = models.CharField(max_length=255)
     correo = models.EmailField(unique=True)
-    contrasena = models.CharField(max_length=100)
+    contrasena = models.CharField(max_length=255)
     fecha_nacimiento = models.DateField()
