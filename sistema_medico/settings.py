4c15ea80a0769923f9ea6f2eba13167cd0fb982a
brianrscode <135724962+brianrscode@users.noreply.github.com>
2025-04-29 08:31:43 -0600

    Cambio nombre de variable para base de datos

--- sistema_medico/settings.py
+++ sistema_medico/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'sistema_medico.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.getenv('MYSQL_URL'))
 }
 # DATABASES = {
 #     'default': {
