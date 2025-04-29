724902f23a9c7b95c326b7459350098276cde281
brianrscode <135724962+brianrscode@users.noreply.github.com>
2025-04-29 16:42:21 -0600

    Modifico la forma de asignar un valor a la variable DEBUG

--- sistema_medico/settings.py
+++ sistema_medico/settings.py
@@ -14,7 +14,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False')
+DEBUG = os.getenv('DEBUG', default=False) == 'True'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'web-production-d8f7d.up.railway.app']
 
