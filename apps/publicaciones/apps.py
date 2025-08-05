c5a262b1ccd12acb12d8ebaa736ac852442ab131
brianrscode <135724962+brianrscode@users.noreply.github.com>
2025-08-05 13:24:58 -0600

    Agrego funcionalidad para eliminar imagen de publicaciones al eliminarlas

--- apps/publicaciones/apps.py
+++ apps/publicaciones/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PublicacionesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.publicaciones'
+
+    def ready(self):
+        import apps.publicaciones.signals
